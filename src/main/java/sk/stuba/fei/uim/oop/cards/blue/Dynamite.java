c8ce35a51d3163ae442941d195ce2b0d640b2253
kocak <streax14@gmail.com>
2023-03-22 19:46:20 +0100

    dynamite add

---
+++ src/main/java/sk/stuba/fei/uim/oop/cards/blue/Dynamite.java
@@ -0,0 +1,38 @@
+package sk.stuba.fei.uim.oop.cards.blue;
+
+import sk.stuba.fei.uim.oop.cards.Card;
+import sk.stuba.fei.uim.oop.player.Player;
+import sk.stuba.fei.uim.oop.table.Table;
+
+import java.util.List;
+
+public class Dynamite extends BlueCard{
+    private static final String CARD_NAME = "Dynamite";
+    public Dynamite() {
+        super(CARD_NAME, 8);
+    }
+
+    @Override
+    public void play(Player playerOnTurn, List<Player> enemies, Table table) {
+        super.play(playerOnTurn, enemies, table);
+        playerOnTurn.activateCard(this);
+    }
+
+    public void explode(Player player){
+        System.out.println("Dynamite exploded in "+player.getName()+"'s hands.");
+        player.removeLive(3);
+        if (!player.isAlive()) {
+            player.die();
+        }
+    }
+
+    @Override
+    public boolean isPlayable(Player currentPlayer, List<Player> enemies) {
+       for(Card card : currentPlayer.getActiveBlueCards()){
+           if(card instanceof Dynamite){
+               return false;
+           }
+       }
+       return true;
+    }
+}
